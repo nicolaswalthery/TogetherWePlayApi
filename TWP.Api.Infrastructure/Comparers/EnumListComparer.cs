5da5b8185ef411f11766ae38206577e025a390f9
nicolaswalthery <nicolaswalthery@protonmail.com>
2024-11-10 07:26:48 +0100

    push

--- TWP.Api.Infrastructure/Comparers/EnumListComparer.cs
+++ TWP.Api.Infrastructure/Comparers/EnumListComparer.cs
@@ -6,7 +6,7 @@ namespace TWP.Api.Infrastructure.Comparers
     public class EnumListComparer<T> : ValueComparer<List<T>>
        where T : Enum
     {
-        public EnumListComparer() : base(EnumListEqualsExpression(), EnumListGetHashCodeExpression()) { }
+        public EnumListComparer() : base(EnumListEqualsExpression()!, EnumListGetHashCodeExpression()) { }
 
         private static Expression<Func<List<T>, List<T>, bool>> EnumListEqualsExpression() => (a, b) => a.SequenceEqual(b);
         private static Expression<Func<List<T>, int>> EnumListGetHashCodeExpression() => a => a.Sum(a => a.GetHashCode());
