1405f5d697152130dcb04d2c817200e2c9ec8aba
Blue-haze <blue-haze@hotmail.com>
2024-12-30 15:12:29 +0100

    push

--- Common/Extensions/FileExtensions.cs
+++ Common/Extensions/FileExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Common.Extensions
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Common.Extensions
 {
     public static class FileExtensions
     {
@@ -6,7 +8,12 @@
         {
             try
             {
-                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $".../{folderName}/{fileName}.json");
+                //string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"/{folderName}/{fileName}.json");
+                string filePath = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\");
+                //Path.GetFullPath(Path.Combine(baseDirectory, @"..\..\..\"))
+                //var jsonSaveFilePath = Directory.GetFiles(Path.Combine(Application.dataPath, $"../Saves/"), $"{saveJsonFileToLoad}.json").FirstOrDefault();
+                //if (String.IsNullOrWhiteSpace(jsonSaveFilePath))
+                //    throw new Exception($"{saveJsonFileToLoad} not found in the save files !");
 
                 string jsonContent = File.ReadAllText(filePath);
 
