de16cc69b527e2324f2790cfc329e4f51713faad
Blue-haze <blue-haze@hotmail.com>
2024-12-30 11:04:46 +0100

    push

---
+++ TWP.Api.Infrastructure/JsonRepositories/Interfaces/IJsonRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TWP.Api.Infrastructure.JsonRepositories.Interfaces
+{
+    public interface IJsonRepository<TReturn>
+    {
+        TReturn Get(string fileName);
+        IEnumerable<TReturn> GetAll(string folderPath);
+    }
+}
---
+++ TWP.Api.Infrastructure/JsonRepositories/MonsterActivitiesJsonRepository.cs
@@ -0,0 +1,17 @@
+﻿using TWP.Api.Infrastructure.JsonRepositories.Interfaces;
+
+namespace TWP.Api.Infrastructure.JsonRepositories
+{
+    public class MonsterActivitiesJsonRepository : IJsonRepository<string>
+    {
+        public string Get(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAll(string folderPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
