a1276df5b7bca74d6b8c03422ed5ce92d356fdc3
Blue-haze <blue-haze@hotmail.com>
2024-12-31 13:59:19 +0100

    push

--- TWP.Api.Infrastructure/JsonRepositories/JsonRepositoryBase.cs
+++ TWP.Api.Infrastructure/JsonRepositories/JsonRepositoryBase.cs
@@ -3,12 +3,12 @@
     public class JsonRepositoryBase
     {
         private string _randomTablesRelativePath = @"TWP.Api.Infrastructure\JsonFiles\RandomTables";
-        private string _relativePath = @"TWP.Api.Infrastructure\JsonFiles";
+        private string _baseRelativePath = @"TWP.Api.Infrastructure\JsonFiles";
 
         public JsonRepositoryBase() { }
 
         protected string RandomTablesRelativePath => _randomTablesRelativePath;
-        protected string BaseFolderRelativePath => _relativePath;
+        protected string BaseFolderRelativePath => _baseRelativePath;
 
     }
 }
