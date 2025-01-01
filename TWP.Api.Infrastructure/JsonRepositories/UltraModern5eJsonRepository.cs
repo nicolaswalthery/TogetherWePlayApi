a151d3098dd2dfad297405d626a7d5b19f437201
Blue-haze <blue-haze@hotmail.com>
2025-01-01 21:11:46 +0100

    push

--- TWP.Api.Infrastructure/JsonRepositories/UltraModern5eJsonRepository.cs
+++ TWP.Api.Infrastructure/JsonRepositories/UltraModern5eJsonRepository.cs
@@ -15,10 +15,35 @@ namespace TWP.Api.Infrastructure.JsonRepositories
         {
         }
 
+        #region Treasure Tables
         public RollTableDto GetTechItemTable_A_RandomTable() 
             => base.GetRollTable(fileName: "TechItemTableARandomTable").ToTechItemTableARandomTableRollTableDto();
 
-        public RollTableDto GetShootAndLootCompanyData()
+        #endregion Treasure Tables
+
+        #region Company Data Tables
+        public RollTableDto GetShootAndLootCompanyName()
             => base.GetRollTable(fileName: "ShootAndLootCompanyData").ToShootAndLootCompanyNameRollTableDto();
+
+        public RollTableDto GetShootAndLootDamageType()
+            => base.GetRollTable(fileName: "ShootAndLootCompanyData").ToShootAndLootDamageTypeRollTableDto();
+
+        public RollTableDto GetShootAndLootMagazine()
+            => base.GetRollTable(fileName: "ShootAndLootCompanyData").ToShootAndLootMagazineRollTableDto();
+
+        public RollTableDto GetShootAndLootTechLevel()
+            => base.GetRollTable(fileName: "ShootAndLootCompanyData").ToShootAndLootTechLevelRollTableDto();
+
+        #endregion Company Data Tables
+        
+        #region Line Data
+
+        public RollTableDto GetShootAndLootLineData()
+            => base.GetRollTable(fileName: "ShootAndLootLineData").ToShootAndLootLineRollTableDto();
+
+        public RollTableDto GetShootAndLootAdditionalProperty()
+            => base.GetRollTable(fileName: "ShootAndLootLineData").ToShootAndLootAdditionalPropertyRollTableDto();
+
+        #endregion Line Data Tables
     }
 }
