1b4824bebb7eadd5beae8d252de411e7a7173229
Nicolas Walthéry <nicolaswalthery@protonmail.com>
2025-06-28 14:04:17 +0200

    llm services

---
+++ TWP.Api.Infrastructure/Interops/LlmServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWP.Api.Infrastructure.Interops
+{
+    internal class LlmServices
+    {
+    }
+}
