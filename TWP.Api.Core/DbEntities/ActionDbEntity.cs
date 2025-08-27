91859f82ac9fec8e6bd3d4d5ae365a0589ffb309
Nicolas Walthéry <nicolaswalthery@protonmail.com>
2025-08-27 10:56:54 +0200

    push

--- TWP.Api.Core/DbEntities/ActionDbEntity.cs
+++ TWP.Api.Core/DbEntities/ActionDbEntity.cs
@@ -6,13 +6,12 @@ namespace TWP.Api.Core.DbEntities
     {
         public Guid MonsterId { get; set; }
         public string Name { get; set; } = string.Empty;
-
         public ActionTypeEnum Type { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public int? AttackBonus { get; set; }
         public DiceTypeEnum? DamageDice { get; set; }
         public int? NumberDamageDice { get; set; }
-        public DamageTypeEnum DamageType { get; set; }
+        public DamageTypeEnum? DamageType { get; set; }
         public int? LimitPerDay { get; set; }
 
         // Navigation Property
