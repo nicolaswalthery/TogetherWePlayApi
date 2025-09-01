c676613ac2c29679d7dc22163bf105dcb10ef42c
Nicolas WALTHERY <P62792@nrb.be>
2025-09-01 13:37:51 +0200

    push

--- TWP.Api.Infrastructure/Repository/Monster5eRepository.cs
+++ TWP.Api.Infrastructure/Repository/Monster5eRepository.cs
@@ -20,8 +20,13 @@ namespace TWP.Api.Infrastructure.Repository.Interfaces
                 return Result<List<Monster5eDbEntity>>.Success(result);
             });
 
-        public async Task<Result> Insert(Monster5eDbEntity monster5EDbEntity)
-            => await Safe.ExecuteAsync(async () => await base.AddAsync(monster5EDbEntity));
+        public async Task<Result> InsertMany(List<Monster5eDbEntity> monster5EDbEntities)
+            => await Safe.ExecuteAsync(async () =>
+            {
+                base._context.AddRangeAsync(monster5EDbEntities);
+                await base._context.SaveChangesAsync();
+                return Result.Success();
+            });
 
     }
 }
