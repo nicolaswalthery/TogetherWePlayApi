9afa6a3fe493025a7363b9f39aada783c4a1de9e
Nicolas Walthéry <nicolaswalthery@protonmail.com>
2025-10-07 19:31:28 +0200

    push

--- TogetherWePlayApi/start.sh
+++ TogetherWePlayApi/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# Construire et démarrer l'application .NET
+# Construire l'application .NET
+echo "Building .NET application..."
 dotnet build
+
+# Vérifier si la commande de build a réussi
+if [ $? -ne 0 ]; then
+  echo "Build failed!"
+  exit 1
+fi
+
+# Démarrer l'application
+echo "Running .NET application..."
 dotnet run
